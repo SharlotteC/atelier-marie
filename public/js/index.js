95be5fb56500025d8cea76e486ddf267b5c3d4c7
Sharlotte.C <103247738+SharlotteC@users.noreply.github.com>
2022-08-22 14:52:01 +0200

    Mise en place css home_page, importation librairie animejs, animation de la home-page avec js

---
+++ public/js/index.js
@@ -0,0 +1,31 @@
+// ------------------------------ animation home-page-----------------------------------
+
+var basicTimeline = anime.timeline();
+
+    basicTimeline
+    .add({
+        targets: "#basicTimeline .first.el",
+        translateX: 1200,
+        easing: "easeOutExpo",
+        delay: 1500,
+    })
+    .add({
+        targets: "#basicTimeline .second.el",
+        translateX: 1200,
+        easing: "easeOutExpo",
+    })
+    .add({
+        targets: "#basicTimeline .third.el",
+        translateX: 1650,
+        easing: "easeOutExpo",
+    })
+    .add({
+        targets: "#basicTimeline .fourth.el",
+        translateX: 1750,
+        easing: "easeOutExpo",
+    })
+    .add({
+        targets: "#basicTimeline .fiveth.el",
+        translateX: 1650,
+        easing: "easeOutExpo",
+    });

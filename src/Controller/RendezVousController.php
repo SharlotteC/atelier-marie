56a5df23af2d64d1de53abe91f97eb8dc90baf7e
Sharlotte.C <103247738+SharlotteC@users.noreply.github.com>
2022-08-29 09:57:44 +0200

    rectification du header + création de l'entité user + création  du formulaire de rdv + mise en place page rdv et css page rdv

--- src/Controller/RendezVousController.php
+++ src/Controller/RendezVousController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Form\UserformType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RendezVousController extends AbstractController
 {
+
     #[Route('/rendez/vous', name: 'rendez_vous')]
-    public function index(): Response
+    public function rdv(Request $request, UserRepository $userRepository)
     {
+
+        $rdvForm = $this->createForm(UserformType::class);
+        $rdvForm->handleRequest($request);
+
+
         return $this->render('rendez_vous/rdv.html.twig', [
-            'controller_name' => 'RendezVousController',
+            'rdv_Form' => $rdvForm->createView(),
         ]);
+
+
     }
 }

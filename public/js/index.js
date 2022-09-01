5013d955e4aeecef3a18f1a37fe3d5d1884b53b2
Sharlotte.C <103247738+SharlotteC@users.noreply.github.com>
2022-09-01 10:10:59 +0200

    mise en place calendrier de reservation page rdv + css calendrier

--- public/js/index.js
+++ public/js/index.js
@@ -29,3 +29,22 @@ var basicTimeline = anime.timeline();
         translateX: 1650,
         easing: "easeOutExpo",
     });
+
+    //-----------------------------------------Calendar----------------------------------
+
+    window.onload = () => {
+        let calendarElt = document.querySelector('#calendrier')
+
+        let calendar = new FullCalendar.Calendar(calendarElt, {
+            initialView: 'dayGridMonth',
+            locale: 'fr',
+            timeZone: 'Europe/Paris',
+            headerToolbar: {
+                start: 'prev',
+                center: 'title',
+                end: 'next'
+            }
+        })
+
+        calendar.render()
+    }
